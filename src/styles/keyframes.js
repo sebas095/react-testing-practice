4baacb700b6b367c07d97b169b9ae3a080fd0b54
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-11-21 03:51:31 -0500

    [ADD] animations

---
+++ src/styles/keyframes.js
@@ -0,0 +1,37 @@
+import { keyframes } from "styled-components";
+
+export const scaleAnimation = keyframes`
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+`;
+
+export const fadeAnimation = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const fadeOutAnimation = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+export const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
